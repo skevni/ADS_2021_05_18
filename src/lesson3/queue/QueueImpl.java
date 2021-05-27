02ddc44216f72c54edb0992d6fe360c560d263de
Evgeniy Sklyarov <skevni@gmail.com>
2021-05-28 01:58:13 +0300

    Стек и очередь
    1. задание TestHW.java
    2. задание ReversData.java
    3. задание Deque.java, DequeImpl.java

--- src/lesson3/queue/QueueImpl.java
+++ src/lesson3/queue/QueueImpl.java
@@ -14,7 +14,7 @@ public class QueueImpl<E> implements Queue<E> {
     @SuppressWarnings("unchecked")
     public QueueImpl(int maxSize) {
         this.data = (E[]) new Object[maxSize];
-        this.head = maxSize;
+        this.head = HEAD_DEFAULT;
         this.tail = TAIL_DEFAULT;
     }
 
