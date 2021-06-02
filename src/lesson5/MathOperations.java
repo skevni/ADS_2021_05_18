45cdf3c477c4a651483a5c8bfa438154ec07aaf0
Evgeniy Sklyarov <skevni@gmail.com>
2021-06-02 22:58:13 +0300

    Algorithms - homework5
    exponentiation implemented

---
+++ src/lesson5/MathOperations.java
@@ -0,0 +1,30 @@
+package lesson5;
+
+public class MathOperations {
+    /*  Метод умеет возводить в целую степень положительные и отрицательные числа
+     *   дробные степени не понял пока что как сделать
+     */
+    public static double power(int num, double power) {
+        if (num < 0 && power < 1 && power > -1)
+            throw new IllegalArgumentException(String.format("Incorrect parameter power(%f) for calculating a fractional power" +
+                    " of a negative number(%d)", power, num));
+
+        if (power % 1 != 0) {
+            throw new IllegalArgumentException(String.format("Calculating of fractional power(%f) is not implemented", power));
+        }
+        if (num == 0)
+            return 0;
+        if (power > 0){
+            return pw(num,power);
+        } else {
+            return (1 / pw(num, power*-1));
+        }
+    }
+
+    private static double pw(int num, double power) {
+        if (power == 0) {
+            return 1;
+        }
+        return num * pw(num, power - 1);
+    }
+}
---
+++ src/lesson5/TestPower.java
@@ -0,0 +1,12 @@
+package lesson5;
+
+public class TestPower {
+    public static void main(String[] args) {
+
+//        System.out.println(toPower(8, 1/3.0));
+//        System.out.println(MathOperations.power(-2, -2.5));
+        System.out.println("-2^-2=\t" + MathOperations.power(-2, -2));
+        System.out.println(" 2^ 7=\t" + MathOperations.power(2, 7));
+    }
+
+}
