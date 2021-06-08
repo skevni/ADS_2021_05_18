a73d7b2ec3503fe3014c5500fcd5a9c7a6a40e62
OKrylov <krylov92oa@gmail.com>
2021-06-08 23:16:27 +0300

    Lesson 7

---
+++ src/lesson7/Graph.java
@@ -0,0 +1,23 @@
+package lesson7;
+
+public interface Graph {
+
+    void addVertex(String label);
+
+    boolean addEdge(String startLabel, String secondLabel, String... others);
+
+    int getSize();
+
+    void display();
+
+    /**
+     * англ. Depth-first search, DFS
+     */
+    void dfs(String startLabel);
+
+    /**
+     * англ. breadth-first search, BFS
+     */
+    void bfs(String startLabel);
+
+}
